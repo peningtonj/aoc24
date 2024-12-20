7941ac12028292b1304649cabf0d61f3ca004386
Joseph Penington <josephpenington@Josephs-Laptop.local>
2024-12-20 17:31:12 +1100

    Day 19

--- Day 19/solution.cpp
+++ Day 19/solution.cpp
@@ -12,217 +12,166 @@
 #include <numeric> // For std::accumulate
 
 
-int width = 5;
-int height = 3;
-int initial_value = 0;
+class TowelDesign {
+    std::vector<std::string> towels;
+    std::vector<std::string> designs;
 
+        void find_combos(std::string towel, std::string remaining, std::vector<std::string> combo) {
 
+            // std::cout << "Trying to make design " << towel << " and remaining is " << remaining << "\n";
 
-struct coord {
-    int x, y;
+            if (remaining == "") {
+                bool bad_combo = false;
+                std::string last_towel = combo.at(combo.size() - 1);
+                for (auto t : towels) {
+                    if ((last_towel != t) && (last_towel == t.substr(0, last_towel.size()))) {
+                        // std::cout << "Issue with " << last_towel << " due to " << t << "\n";
+                        bad_combo = true;
+                        break;
+                    }
+                }
 
-    void print() {
-        std::cout << x << ", " << y << "\n";
-    }
+                if (!bad_combo && (combo != std::vector{towel})) composite_towels.at(towel).push_back(combo);
+                return;
+            }
 
-    bool operator==(const coord& other) const {
-        return x == other.x && y == other.y;
-    }
+            for (auto next_towel : towels) {
+                if (next_towel == remaining.substr(0, next_towel.size())) {
+                        // std::cout << "Making profress with " << next_towel << "\n";
+                        std::vector<std::string> running_combo = combo;
+                        running_combo.push_back(next_towel);
+                        find_combos(towel, remaining.substr(next_towel.size(), remaining.size()), running_combo);
+                    }
+                }
+            return;
+        } 
 
-    coord operator+(const coord& other) const {
-        return coord{x + other.x, y + other.y};
-    }
-
-    bool operator<(const coord& other) const {
-        return (x < other.x) || (x == other.x && y < other.y);
-    }
-
-    std::vector<coord> get_adj() {
-        return {
-            coord{x, y + 1},
-            coord{x, y - 1},
-            coord{x + 1, y},
-            coord{x - 1, y},
-        };
-    }
-};
-
-namespace std {
-    template <>
-    struct hash<coord> {
-        size_t operator()(const coord& c) const {
-            // Combine the hash of x and y
-            return std::hash<int>{}(c.x) ^ (std::hash<int>{}(c.y) << 1);
+    void make_composite_towels() {
+        for (auto towel : towels) {
+            composite_towels[towel] = {{towel}};
+            find_combos(towel, towel, {});
         }
-    };
-}
-
-coord NULL_COORD = coord{-1, -1};
-
-template <typename T>
-bool contains(const std::vector<T>& elements, const T& target) {
-    return std::find(elements.begin(), elements.end(), target) != elements.end();
-}
-
-class MemoryMap {
-    int width, height;
-    std::unordered_map<coord, std::vector<std::pair<coord, int>>> adjList;
-    std::map<coord, coord> prev;      // Predecessors for path reconstruction
-    std::vector<std::vector<coord> > paths;
-    
-    bool point_in_maze(coord point) {
-        return (point.x >= 0) and (point.x < width) and (point.y >= 0) and (point.y < height);
     }
 
     public:
-        std::map<coord, std::vector<coord>> all_prev;      // Predecessors for path reconstruction
-        coord end;
-        std::map<coord, int> dist;       // Distance from start node
+        std::map<std::string, std::vector<std::vector <std::string> > >composite_towels;
+        std::map<std::string, bool> skippable;
+        std::map<std::string, long long> cache;
 
-        std::vector<coord> falling;
-        MemoryMap(int width, int height, int lapsed, std::string input_file) {
-            this->width = width;
-            this->height = height;
-            end = coord{width - 1, height - 1};
-
+        TowelDesign(std::string input_file) {
             std::string myText;
             std::ifstream MyReadFile(input_file);
 
-            int x, y;
-            char c;
+            std::string line;
+            std::getline(MyReadFile, line);
 
-            while ((MyReadFile >> x >> c >> y) && lapsed)
-            {
-                falling.push_back(coord{x, y});
-                lapsed -= 1;
+            size_t pos = 0;
+            while ((pos = line.find(",")) != std::string::npos) {
+                towels.push_back(line.substr(0, pos));
+                line.erase(0, pos + 2);
             }
-            MyReadFile.close();
+            towels.push_back(line);
+            std::getline(MyReadFile, line); // Flush empty line
 
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < width; y++ ) {
-                    if(!contains(falling, coord{x, y})) {
-                        // coord{x, y}.print();
-                        for (auto adj: coord{x, y}.get_adj()) {
-                            if (!contains(falling, adj) && point_in_maze(adj)) 
-                                adjList[coord{x, y}].push_back({adj, 1});
-                        }
-                    }
-                }
+            while (std::getline(MyReadFile, line)) {
+                designs.push_back(line);
             }
+
+            make_composite_towels();
+
+            // for (auto [key, value]: composite_towels) {
+            //     std::cout << key << ": \n";
+            //     for (auto v : value) {
+            //         for (auto t : v ) {
+            //             std::cout << t << ", ";
+            //         }
+            //         std::cout << "\n";
+            //     }
+            // }
+
+            std::sort(towels.begin(), towels.end(), [](const std::string& a, const std::string& b){
+                return a.size() > b.size();
+            });            
         }
 
-        void dj(coord start) {
-            std::set<coord> visited;         // Set to track visited nodes
 
-            // std::cout << "Calculating paths from ";
-            // Initialize all distances to "infinity"
-            for (const auto& n : adjList) {
-                dist[n.first] = INT_MAX;
-                prev[n.first] = NULL_COORD;
-            }
+
+        bool make_design(std::string design) {
             
-            // Lambda for priority queue comparison (min-heap)
-            auto compare = [this](const coord& a, const coord& b) { return dist.at(a) > dist.at(b); };
-            std::priority_queue<coord, std::vector<coord>, decltype(compare)> pq(compare);
+            // std::cout << "Trying to make design " << design << "\n";
 
-            // Start with the source node
-            dist[start] = 0;
-            pq.push(start);
+            auto it = std::find(towels.begin(), towels.end(), design);
+            if (it != towels.end()) return true;
 
-            // Dijkstra's main loop
-            while (!pq.empty()) {
-                coord u = pq.top();
-                pq.pop();
-
-                if (visited.find(u) != visited.end()) continue; // Skip if already visited
-                visited.insert(u);
-
-                // std::cout << "#######\n";
-                // u.print();
-                // Check neighbors (edges originating from u)
-                for (const auto& n : adjList[u]) {
-                    coord v = n.first;
-                    // v.print();
-                    int weight = n.second;
-                    int alt = dist[u] + weight;
-                    if (alt < dist[v]) { // Relaxation
-                        dist[v] = alt;
-                        prev[v] = u;
-                        pq.push(v);
+            for (auto towel : towels) {
+                // std::cout << towel << "-" << towel.size() <<  " - " << design.substr(0, towel.size()) << "\n";
+                if (towel == design.substr(0, towel.size())) {
+                    if (make_design(design.substr(towel.size(), design.size()))) {
+                        return true;
                     }
                 }
             }
-    }
 
-    std::vector<coord> reconstruct_path() {
-            // Path reconstruction
-        std::vector<coord> path;
-        coord current = end;
-        while (current != coord{0, 0} && current != NULL_COORD) {
-            path.push_back(current);
-            current = prev[current];
+            return false;
+        } 
+
+        int makable_designs() {
+            int result = 0;
+            for (auto d : designs) {
+                result += make_design(d);
+            }
+            return result;
         }
-        if (current == NULL_COORD) return path;
-        path.push_back(coord{0, 0});
-        std::reverse(path.begin(), path.end());
-        return path;
-    }
 
-        coord check_paths(int lapsed, std::string input_file) {
-            std::string myText;
-            std::ifstream MyReadFile(input_file);
+        long long count_design(std::string design) {
 
-            int x, y;
-            char c;
+            if (design == "") return 1;
+            if (cache.contains(design)) return cache[design];
 
-            while (lapsed)
-            {
-                MyReadFile >> x >> c >> y;
-                lapsed -= 1;
-            }
-            
-            dj(coord{0, 0});
-            std::vector path = reconstruct_path();
-
-            int limit = 10;
-            while ((path.at(0) == coord{0,0}) && limit) {
-                MyReadFile >> x >> c >> y;
-                    for (auto& [key, adj] : adjList) { 
-                        for (int i = 0; i < adj.size(); i++ ) {
-                            if (adj.at(i).first == coord{x, y}) {
-                                adj.erase(adj.begin() + i);
-                            }
-                        }
+            long long result = 0;
+            for (auto towel : towels) {
+                if (towel == design.substr(0, towel.size())) {
+                        result += count_design(design.substr(towel.size(), design.size()));
                     }
-                if (contains(path, coord{x, y})) {
-                    dj(coord{0, 0});
-                    path = reconstruct_path();
                 }
-            } 
-        return coord{x, y};
+
+            cache[design] = result;
+            return result;
+
+        }
+
+        long long count_designs() {
+            long long result = 0;
+            for (auto d : designs) {
+                    result += count_design(d);
+            }
+
+            return result;
         }
 
 
 };
 
-int part_one(MemoryMap mem) {
-    mem.dj(coord{0, 0});
-    return mem.dist.at(coord{mem.end});
+int part_one(TowelDesign td) {
+    return td.makable_designs();
 }
 
-coord part_two(MemoryMap mem) {
-    return mem.check_paths(1024, "input.txt");
+long long part_two(TowelDesign td) {
+    
+    return td.count_designs();
+    // return 0;
 }
 
 int main() {
 
-    MemoryMap mem = MemoryMap(71, 71, 1024, "input.txt");
+    TowelDesign td = TowelDesign("input.txt");
 
     std::cout << "Part One:\n";
-    std::cout << part_one(mem);
+    std::cout << part_one(td);
 	
     std::cout << "\nPart Two:\n";
-    part_two(mem).print();
+    std::cout << part_two(td);
+    
 
     return 0;
 
